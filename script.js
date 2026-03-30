3a44fac7d0e31442edf4d85ad212879cf02c2f8e
DavidVinent <dvinent20628@iesjoanramis.org>
2026-03-30 14:01:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,277 @@
+let canvas;
+let ctx;
+let FPS = 50;
+
+let ampleC = 50;
+let altC = 50;
+
+let cesped = '#006600'
+let aigua = '#0066CC'
+let terra = '#311900'
+let pedra = '#606060'
+let clau = '#fbff00'
+let porta = '#682f00'
+
+let retras = 0;
+
+let tilemap
+
+let escenari = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 3, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+
+
+
+
+
+]
+
+function dibuixaEscenari() {
+    for (let y = 0; y < 32; y++) {
+        for (let x = 0; x < 48; x++) {
+            let tile = escenari[y][x]
+            ctx.drawImage(tilemap, tile * 32, 0, 32, 32, x * ampleC, y * altC, ampleC, altC)
+        }
+    }
+}
+
+let imatge;
+let imatge1;
+
+//INNICIALITZA ES JOC UNA VEGADA
+function inicializar() {
+    canvas = document.getElementById("pam talla");
+    ctx = canvas.getContext('2d');
+    imatge = new Image()
+    imatge.src = './agrybird.png'
+    imatge1 = new Image()
+    tilemap = new Image()
+    imatge1.src = './hongo.png'
+    tilemap.src = './p.png'
+    //INTERVAL 50 VEGADES PER SEGON
+    setInterval(function () {
+        principal();
+    }, 1000 / FPS)
+}
+
+//MODIFICA PANTALLA
+function borrarPantalla() {
+    canvas.width = 2400
+    canvas.heigth = 1600
+}
+
+
+
+
+
+
+let prota = function (x, y, vel) {
+    this.x = x;
+    this.y = y;
+    this.clau = false;
+
+    this.muerte = function (x, y) {
+        let autista = false;
+        if (this.x == x && this.y == y) {
+            autista = true;
+        }
+        return autista
+    }
+
+    this.dibuixa = function () {
+        ctx.drawImage(imatge, this.x, this.y, 50, 50)
+    }
+    this.text = function () {
+        ctx.font = '30px impact'
+        ctx.fillStyle = '#555555'
+        ctx.fillText("X: " + this.x + " Y: " + this.y, 50, 50)
+    }
+    this.margenes = function (x, y) {
+        let colisio = false;
+        if (escenari[y / 50][x / 50] == 0 || escenari[y / 50][x / 50] == 4) {
+            colisio = true;
+
+            return colisio;
+        }
+        return colisio;
+    }
+    this.logica = function () {
+        if (escenari[this.y / 50][this.x / 50] == 3) {
+            this.clau = true;
+            retras = 15
+            alert("VAMOS")
+            escenari[this.y / 50][this.x / 50] = 2
+        }
+        if (escenari[this.y / 50][this.x / 50] == 1) {
+            if (this.clau) {
+                alert("puto amo")
+            } else (alert("cerrado bobi"))
+        }
+    }
+    this.adalt = function () {
+        if (!this.margenes(this.x, this.y - 50)) { this.y = this.y - 50 }
+        this.logica()
+
+    }
+    this.baix = function () {
+        if (!this.margenes(this.x, this.y + 50)) { this.y = this.y + 50 }
+        this.logica()
+    }
+
+    this.dreta = function () {
+        if (!this.margenes(this.x + 50, this.y)) { this.x = this.x + 50 }
+        this.logica()
+
+    }
+    this.esquerra = function () {
+        if (!this.margenes(this.x - 50, this.y)) { this.x = this.x - 50 }
+        this.logica()
+    }
+}
+
+
+
+
+
+
+let enemic = function (x, y, vel) {
+    this.x = x;
+    this.y = y;
+    this.vel = vel;
+
+    this.dibuixa = function () {
+
+        ctx.drawImage(imatge1, this.x, this.y, 50, 50)
+    }
+    this.margenes = function (x, y) {
+        let colisio = false;
+        if (escenari[y / 50][x / 50] == 0 || escenari[y / 50][x / 50] == 4) {
+            colisio = true;
+
+            return colisio;
+        }
+        return colisio;
+    }
+
+    this.mou = function () {
+        let hamuerto = sophie.muerte(this.x, this.y)
+        if (hamuerto) {
+            alert("Que malo eres chaval")
+            sophie.x = 200
+            sophie.y = 250
+        }
+
+        retras++;
+        if (retras == 50) {
+            retras = 0
+            let posicio = Math.floor(Math.random() * 4)
+            if (posicio == 0) {
+                if (!this.margenes(this.x - 50, this.y))
+                    this.x = this.x - 50
+            }
+            if (posicio == 1) {
+                if (!this.margenes(this.x + 50, this.y))
+                    this.x = this.x + 50
+            }
+            if (posicio == 2) {
+                if (!this.margenes(this.x, this.y - 50))
+                    this.y = this.y - 50
+            }
+            if (posicio == 3) {
+                if (!this.margenes(this.x, this.y + 50))
+                    this.y = this.y + 50
+            }
+
+
+
+        }
+    }
+}
+
+
+
+let bauti = new enemic(500, 1200, 40);
+let aito = new enemic(1500, 500, 40)
+let ismailentanga = new enemic(1500, 1000, 40);
+let sophie = new prota(2000, 250);
+let cristianperu = new enemic(1900, 700, 40)
+let cristianbolivia = new enemic(1900, 1300, 40)
+let cristiancolombia = new enemic(1700, 600, 40)
+
+
+function principal() {
+    borrarPantalla();
+    dibuixaEscenari();
+    sophie.dibuixa();
+    bauti.dibuixa()
+    bauti.mou()
+    aito.dibuixa()
+    aito.mou()
+    ismailentanga.dibuixa()
+    ismailentanga.mou()
+    if (sophie.clau) {
+        cristianperu.dibuixa()
+        cristianperu.mou()
+        cristianbolivia.dibuixa()
+        cristianbolivia.mou()
+        cristiancolombia.dibuixa()
+        cristiancolombia.mou();
+    }
+
+
+}
+
+
+document.addEventListener('keydown', function (tecla) {
+    if (tecla.key == 'ArrowUp') {
+        sophie.adalt()
+
+    }
+    if (tecla.key == 'ArrowDown') {
+        sophie.baix()
+
+    }
+    if (tecla.key == 'ArrowRight') {
+        sophie.dreta()
+
+    }
+    if (tecla.key == 'ArrowLeft') {
+        sophie.esquerra()
+
+    }
+})
+
+
